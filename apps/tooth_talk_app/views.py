4998a8fe115856d091999d41f7bc630ccee9cc22
Ebubeukeka <54153295+Ebubeukeka@users.noreply.github.com>
2020-07-27 16:26:59 -0500

    Added videos to 3-5 and high for educational guide

--- apps/tooth_talk_app/views.py
+++ apps/tooth_talk_app/views.py
@@ -17,18 +17,6 @@ def guide_high(request):
     return render(request, 'guide_high.html')
 
 def contact_us(request):
-#     if request.method == 'POST':
-#         form = ContactForm(request.POST)
-#         if form.is_valid():
-#             sender_name = form.cleaned_data['name']
-#             sender_email = form.cleaned_data['email']
-#             message = "{0} has sent you a new message:\n\n{1}".format(sender_name, form.cleaned_data['message'])
-#             send_mail('New Enquiry', message, sender_email, ['enquiry@exampleco.com'])
-#             return HttpResponse('Thanks for contacting us!')
-#     else:
-#         form = ContactForm()
-
-# add {'form': form} in render after html file
     return render(request, 'contact_us.html')
 
 
